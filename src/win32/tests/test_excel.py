24f5b17d4b7c5177c388f72bcbbcaf6109ee0436
mugwort_rc <mugwort.rc@gmail.com>
2015-07-27 13:51:08 +0900

    Add windows_only.

--- src/win32/tests/test_excel.py
+++ src/win32/tests/test_excel.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from .. import excel
+import os
 
+import pytest
+windows_only = pytest.mark.skipif("os.name != 'nt'")
 
+if os.name == "nt":
+    from .. import excel
+
+@windows_only
 def test_com_range():
     assert list(excel.com_range(3)) == [1, 2, 3]
     assert list(excel.com_range(5)) == [1, 2, 3, 4, 5]
 
 
+@windows_only
 def run_test_byUsedRange(data):
     df = excel.byUsedRange(data)
     assert df.columns.values.tolist() == ['ID', 'A', 'B', 'C']
@@ -15,6 +22,7 @@ def run_test_byUsedRange(data):
     assert df.ix[0].values.tolist() == [1, 2, 3, 4]
 
 
+@windows_only
 def test_byUsedRange_list():
     data = [
         ['ID', 'A', 'B', 'C'],
@@ -24,6 +32,7 @@ def test_byUsedRange_list():
     run_test_byUsedRange(data)
 
 
+@windows_only
 def test_byUsedRange_tuple():
     data = (
         ('ID', 'A', 'B', 'C'),
