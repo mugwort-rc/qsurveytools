58788f4270f54c58981ff80a2fdce299822e6f7f
mugwort_rc <mugwort.rc@gmail.com>
2018-10-15 13:38:54 +0900

    Update docs/gen_html.py; Python3 conversion.

--- docs/gen_html.py
+++ docs/gen_html.py
@@ -16,37 +16,37 @@ def locate(pattern, root_path):
 
 def main(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default=os.path.join(BASEDIR, 'md/'))
-    parser.add_argument('--output', default=os.path.join(BASEDIR, 'html/html/'))
+    parser.add_argument("--input", default=os.path.join(BASEDIR, "md/"))
+    parser.add_argument("--output", default=os.path.join(BASEDIR, "html/html/"))
     args, unknown = parser.parse_known_args(args)
 
     if not os.path.isdir(args.input):
-        print('input: "{}" is not directory.'.format(args.input))
+        print("input: {!r} is not directory.".format(args.input))
         return 1
     if not os.path.isdir(args.output):
-        print('output: "{}" is not directory.'.format(args.output))
+        print("output: {!r} is not directory.".format(args.output))
         return 1
 
     from jinja2 import Environment, FileSystemLoader
-    env = Environment(loader=FileSystemLoader(os.path.join(BASEDIR, 'templates'), encoding='utf8'))
-    tpl = env.get_template('template.html')
+    env = Environment(loader=FileSystemLoader(os.path.join(BASEDIR, "templates"), encoding="utf8"))
+    tpl = env.get_template("template.html")
     import markdown
     md = markdown.Markdown(
         extensions=[
-            'markdown.extensions.attr_list',
-            'markdown.extensions.toc',
+            "markdown.extensions.attr_list",
+            "markdown.extensions.toc",
             "markdown.extensions.meta",
         ]
     )
-    for path, file in locate('*.md', args.input):
-        data = open(os.path.join(path, file)).read().decode('utf-8')
+    for path, file in locate("*.md", args.input):
+        data = open(os.path.join(path, file), encoding="utf-8").read()
         html = tpl.render({
-            'html': md.convert(data),
+            "html": md.convert(data),
             "meta": md.Meta if hasattr(md, "Meta") else {},
         })
         distpath = args.output + path[len(args.input):]
-        open(os.path.join(distpath, file[:-2]+'html'), 'w').write(html.encode('utf-8'))
+        open(os.path.join(distpath, file[:-2]+"html"), "w", encoding="utf-8").write(html)
     return 0
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv))
